2e62b5d7023af201fb788cc108d4c9431182e005
Andiiiiie <andiejessica2020@gmail.com>
2023-11-02 18:19:56 +0300

    commit

---
+++ src/main/java/com/example/rh_test/RhTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.rh_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RhTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rh_test/RhTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rh_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
